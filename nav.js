0ec9c4f0521b9f658c5fd54ba775d98ccb68101a
Artia Aryatama <artyaaryatama21@gmail.com>
2022-08-02 22:59:45 +0800

    toggle not working

--- nav.js
+++ nav.js
@@ -1,6 +1,11 @@
 const menuToggle = document.querySelector('.menu-toggle input');
 const nav = document.querySelector('nav ul');
+const menuMap = document.querySelector('.menu-sidebar input')
+const navMap = document.querySelector('.nav-maps ul')
 
 menuToggle.addEventListener('click', function() {
   nav.classList.toggle('slide');
+})
+menuMap.addEventListener('click', function() {
+  navMap.classList.toggle('slide-map');
 })
