0b993d7017462fe3d66239ab365e465c07826927
Artia Aryatama <artyaaryatama21@gmail.com>
2023-03-26 13:57:08 +0800

    add icon

--- resources/views/Main/Include/Style.blade.php
+++ resources/views/Main/Include/Style.blade.php
@@ -6,3 +6,5 @@
 <!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 <!-- <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/unicons.css"> -->
+
+<link rel="shortcut icon" type="image/jpg" href="../../../punlic_html/assets/Main/images/Logo-Sulsel.ico"/>
