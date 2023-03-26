3a47431b9e33c85e1460ef47cafd49203265a34b
Reinhart-Wibisono-Soplantila <reinhartsoplantila@gmail.com>
2023-03-27 00:01:32 +0800

    perbaikan icon tab browser

--- resources/views/Main/Layout/RegulasiLayout.blade.php
+++ resources/views/Main/Layout/RegulasiLayout.blade.php
@@ -10,6 +10,7 @@
     <!-- CSS -->
     <link rel="stylesheet" href="{{URL::asset('assets/Main/style/index.css')}}">
 
+    <link rel="icon" href="{{URL::asset('assets/Main/images/Logo-Sulsel.ico')}}">
     <title>SIMTARU</title>
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
